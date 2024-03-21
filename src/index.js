b53c932facb6d7a521b0ea964f7fbc9d9a2a36b1
fox <foxwho@aliyun.com>
2024-03-21 10:21:45 +0800

    add

--- src/index.js
+++ src/index.js
@@ -9,17 +9,12 @@ import TableOptions from './components/TableOptions.vue';
 import {designerForm} from './utils/form';
 import FcEditor from '@form-create/component-wangeditor';
 import './style/index.css';
-import draggable from 'vuedraggable/src/vuedraggable';
+import draggable from 'vuedraggable';
 import unique from '@form-create/utils/lib/unique';
 import {makeOptionsRule} from './utils/index';
 import formCreate from './utils/form';
-import globalUseLocale, {t} from "./utils/locale";
-
-const addComponent = (id, component) => {
-    designerForm.component(id, component);
-    formCreate.component(id, component);
-}
-
+import ColorPicker from './components/ColorPicker.vue'
+import CustomAlert from './components/CustomAlert.vue';
 
 designerForm.component('draggable', draggable);
 designerForm.component('DragTool', DragTool);
@@ -29,36 +24,37 @@ designerForm.component('Struct', Struct);
 designerForm.component('Fetch', Fetch);
 designerForm.component('Required', Required);
 designerForm.component('TableOptions', TableOptions);
-addComponent('FcEditor', FcEditor);
+designerForm.component('ColorPicker', ColorPicker);
+designerForm.component('CustomAlert', CustomAlert);
+designerForm.component('FcEditor', FcEditor);
+formCreate.component('FcEditor', FcEditor);
+
 
 designerForm.register('_fc', {
-    load(fc, rule) {
-        rule._id = unique();
-        // if (fc.repeat)
-        //     rule.field = unique();
-        // if (fc.value) {
-        //     rule.effect._fc = false;
-        // }
-    }
+  init(fc, rule) {
+      rule._id = unique();
+      if (fc.repeat)
+          rule.field = unique();
+      if (fc.value) {
+          rule.effect._fc = false;
+      }
+  }
 });
 
 designerForm.register('_fc_tool', {
-    load(_, rule) {
-        rule.props.unique = unique();
-    }
+  init(_, rule) {
+      rule.props.unique = unique();
+  }
 });
 
 const install = function (Vue) {
-    Vue.component('FcDesigner', FcDesigner);
+  Vue.component('FcDesigner', FcDesigner);
 };
 
 FcDesigner.install = install;
 FcDesigner.makeOptionsRule = makeOptionsRule;
 FcDesigner.formCreate = formCreate;
-FcDesigner.designerForm = designerForm;
-FcDesigner.component = addComponent;
-FcDesigner.useLocale = globalUseLocale;
-FcDesigner.t = t;
+FcDesigner.designerForm = designerForm
 
 export default FcDesigner;
 
