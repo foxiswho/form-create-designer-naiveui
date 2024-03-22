ee54080f68e6d64faffb552c81cb0f55a47bd8a4
fox <foxwho@aliyun.com>
2024-03-22 16:54:29 +0800

    打包

--- src/utils/index.js
+++ src/utils/index.js
@@ -95,6 +95,7 @@ const _toJSON = {
         return '{\n ' + json.join(',\n ') + '\n}';
     },
     function: function (val) {
+        val = '' + val;
         var exec = (/^ *([\w]+) *\(/).exec(val);
         if (exec && exec[1] !== 'function') {
             return 'function ' + val;
@@ -127,14 +128,16 @@ const _toJSON = {
 };
 
 export const deepParseFn = function (target) {
-    for (let key in target) {
-        if (Object.prototype.hasOwnProperty.call(target, key)) {
-            let data = target[key];
-            if (Array.isArray(data) || is.Object(data)) {
-                deepParseFn(data);
-            }
-            if (is.String(data)) {
-                target[key] = parseFn(data);
+    if(target && typeof target === 'object'){
+        for (let key in target) {
+            if (Object.prototype.hasOwnProperty.call(target, key)) {
+                let data = target[key];
+                if (Array.isArray(data) || is.Object(data)) {
+                    deepParseFn(data);
+                }
+                if (is.String(data)) {
+                    target[key] = parseFn(data);
+                }
             }
         }
     }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 可以动态处理html文件内容的
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from 'path'
-
+const extnedsPlugins = [];
 // https://vitejs.dev/config/
-export default defineConfig({
-  //publicDir: 'public',  // 静态资源服务的文件夹, 默认"public"
-  plugins: [vue()],
+export default defineConfig(({ command, mode })=>{
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  if ( 'build'=== command) {
+    console.log("生产环境")
+  }else if ( 'serve'=== command) {
+    console.log("开发环境")
+    extnedsPlugins.push(createHtmlPlugin({
+      inject:{
+        data:{
+          // 定义了一个title 变量，可以被html中进行引用
+          title:env.VITE_APP_TITLE,
+        }
+      }
+    }))
+  }
+  return {
+    // vite 配置
+    define: {
+      __APP_ENV__: JSON.stringify(env),
+    },
+    plugins: [vue(), ...extnedsPlugins]}
 })
